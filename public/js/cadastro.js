f1f4d4be054ecb3a84506461a6a2f35a1a06e292
Guilherme Nunes Mingardi <guilherme.mingardi@easynvest.com.br>
2019-11-01 15:16:56 -0300

    Adicina pop-up na tela de pessoa juridica

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -1,13 +1,14 @@
+let body = document.querySelector('.body')
+
+let popUp = document.querySelector(".pop-up")
+
+
+
 let btnCadastro = document.querySelector(".action-cadastro")
 btnCadastro.addEventListener('click', concluirCadastro)
 
 let btnVoltar= document.querySelector(".btn-back-cadastro")
 btnVoltar.addEventListener('click', voltar)
-
-let body = document.querySelector('.body')
-
-let popUp = document.querySelector(".pop-up")
-
 
 function concluirCadastro(){
     body.classList.add("overflow-hidden")
